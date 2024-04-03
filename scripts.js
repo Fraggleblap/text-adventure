c2fcf17b387cb47c5823e68a7b54c90345c1809c
Fraggleblap <143848598+Fraggleblap@users.noreply.github.com>
2024-04-03 14:50:48 -0700

    more work

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
-const requestURL = `/text.json`;
-const request = new Request(requestURL);
+let dialogue
 
-const response = await fetch(request);
-const dialogue = await response.json();
+// Fetch json file which contains data for all dialogue and text and responses
+function getDialogue() {
+    fetch("./text.json")
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error
+                (`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+    })
+    .then((data) =>
+        //dialogue = data)
+       doSomething(data))
+    .catch((error) =>
+        console.error("Unable to fetch data:", error));
 
-document.addEventListener("DOMContentLoaded", () => {
-    
-    
-function startAdventure() {
-    document.getElementById("startButton")
-    document.createElement("p").innerText = dialogue.text;
+}
+function doSomething(data) {
+    dialogue = data
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log(`test`)
+    //let data = getDialogue()
+    console.log(getDialogue())
 });
+
+function startAdventure() {
+    console.log("started")
+    document.createElement("p").innerText = dialogue;
+}
