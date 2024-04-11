f7d274fb75e6847a73caebd3fbe8e32809af8897
Fraggleblap <143848598+Fraggleblap@users.noreply.github.com>
2024-04-11 15:31:55 -0700

    Got the story and buttons working

--- scripts.js
+++ scripts.js
@@ -26,40 +26,58 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(`test`)
 });
 
-function changeDial(tempPath) {
+function changeDial(tempPath, previousResponse) {
     if (document.getElementById(`startButton`) !== null) {
         document.getElementById(`startButton`).remove();
     }
     // console.log("started")
     // console.log(dialogue)
     // console.log(dialogue[tempPath].text);
-    let goTo =  dialogue[tempPath].goTo;
+
     document.getElementById(`textArea`).innerText = dialogue[tempPath].text
 
-    for (i = 1; i < dialogue[tempPath].responses+1; i++) {
-        //console.log(dialogue[tempPath].responses)
 
-        let choice = `response` + i + `Choice`;
-        //console.log(choice);
-        if (document.getElementById(`response` + i)) {
-            document.getElementById(`response` + i).remove()
+    if (previousResponse !== undefined) {
+        document.getElementById(`choiceBox`).innerText = previousResponse
+    }
+
+    for (let v = 0; v < 4; v++) {
+        if (document.getElementById(`response` + v) !== null) {
+            document.getElementById(`response` + v).remove()
         }
+    }
 
-        addElement(`button`, `response` + i, dialogue[tempPath][choice], `textArea`, goTo)
+    if (dialogue[tempPath].responses === null) {
+        addElement(`button`, `response1`, `Move on`, `textArea`, dialogue[tempPath], goTo)
+    } else {
+        for (i = 1; i < dialogue[tempPath].responses + 1; i++) {
+            //console.log(dialogue[tempPath].responses)
+            let goTo = `goTo` + i;
+            console.log(goTo);
+            goTo = dialogue[tempPath][goTo];
+            console.log(goTo);
+            if (i === 2 && goTo === undefined) {
+                goTo = dialogue[tempPath].goTo1
+            }
+
+            let choice = `response` + i + `Choice`;
+            //console.log(choice);
+            addElement(`button`, `response` + i, dialogue[tempPath][choice], `textArea`, dialogue[tempPath], goTo)
+        }
     }
 }
 
 
-function addElement(tag, id, content, afterEl, goTo) {
+function addElement(tag, id, content, afterEl, path, goTo) {
     // create a new div element
     const newDiv = document.createElement(tag);
     newDiv.id = id;
-    newDiv.addEventListener("click", (e) => changeDial(goTo));
+    newDiv.class = "button"
+    newDiv.addEventListener("click", (e) => changeDial(goTo, path[id]));
 
     // and give it some content
     //const newContent = document.createTextNode(content);
     newDiv.innerText = content;
-    document.getElementById(`choiceBox`).innerText
 
     // add the text node to the newly created div
     //newDiv.appendChild(newContent);
