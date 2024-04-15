e5472bb31b3412fca3b2cf683b0536f621626dde
Fraggleblap <143848598+Fraggleblap@users.noreply.github.com>
2024-04-15 15:41:15 -0700

    Added in note functionality

--- scripts.js
+++ scripts.js
@@ -25,13 +25,13 @@ function doSomething(data) {
 
 document.addEventListener("DOMContentLoaded", () => {
     getDialogue();
-    console.log(`test`)
 });
 
 function changeDial(tempPath, previousResponse) {
     if (document.getElementById(`startButton`) !== null) {
         document.getElementById(`startButton`).remove();
     }
+
     // console.log("started")
     // console.log(dialogue)
     // console.log(dialogue[tempPath].text);
@@ -43,7 +43,7 @@ function changeDial(tempPath, previousResponse) {
         document.getElementById(`choiceBox`).innerText = previousResponse
     }
 
-    for (let v = 0; v < 4; v++) {
+    for (let v = 0; v < 5; v++) {
         if (document.getElementById(`response` + v) !== null) {
             document.getElementById(`response` + v).remove()
         }
@@ -51,8 +51,15 @@ function changeDial(tempPath, previousResponse) {
 
     if (dialogue[tempPath].responses === undefined) {
         let goTo = dialogue[tempPath].goTo1
+        if (dialogue[tempPath].needNote !== undefined && noteFound === true) {
+            goTo = dialogue[tempPath].goTo2
+        }
         addElement(`button`, `response1`, `Move on`, `textArea`, dialogue[tempPath], goTo)
     } else {
+        if (dialogue[tempPath].getNote !== undefined) {
+            noteFound = true;
+            console.log("note found");
+        }
         for (i = 1; i < dialogue[tempPath].responses + 1; i++) {
             //console.log(dialogue[tempPath].responses)
             let goTo = `goTo` + i;
@@ -72,21 +79,16 @@ function changeDial(tempPath, previousResponse) {
 
 
 function addElement(tag, id, content, afterEl, path, goTo) {
-    // create a new div element
-    const newDiv = document.createElement(tag);
-    newDiv.id = id;
-    newDiv.class = "button"
-    newDiv.addEventListener("click", (e) => changeDial(goTo, path[id]));
+    // create a new element
+    const newEl = document.createElement(tag);
+    newEl.id = id;
+    newEl.class = "button"
+    newEl.addEventListener("click", (e) => changeDial(goTo, path[id]));
 
     // and give it some content
-    //const newContent = document.createTextNode(content);
-    newDiv.innerText = content;
-
-    // add the text node to the newly created div
-    //newDiv.appendChild(newContent);
-
+    newEl.innerText = content;
 
     // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById(afterEl);
-    currentDiv.after(newDiv);
+    const currentEl = document.getElementById(afterEl);
+    currentEl.after(newEl);
 }
