5ead99f039895b3360d374a9ad7736801582991b
Fraggleblap <143848598+Fraggleblap@users.noreply.github.com>
2024-04-17 14:44:41 -0700

    Added images

--- scripts.js
+++ scripts.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
     getDialogue();
 });
 
-function changeDial(tempPath, previousResponse) {
+function changeDial(tempPath, previousResponse, image) {
     if (document.getElementById(`startButton`) !== null) {
         document.getElementById(`startButton`).remove();
     }
@@ -38,8 +38,20 @@ function changeDial(tempPath, previousResponse) {
 
     document.getElementById(`textArea`).innerText = dialogue[tempPath].text
 
+    if (image !== undefined) { // if image exists, add styling to image element and set it's src
+        document.getElementById(`imageBox`).style = `   display: flex;
+        justify-content: center;
+        align-self: center;
+        width: 50%;
+        border-radius: 3px;
+        border-color: blueviolet;
+        border-style: solid;`
 
-    if (previousResponse !== undefined) {
+        document.getElementById(`imageBox`).src = `/images/`+image+`.png`
+    } else { // otherwise remove styling so there is no random purple bar
+        document.getElementById(`imageBox`).style = ``
+    }
+    if (previousResponse !== undefined) { // set the previous text into a text area so the player can see their previous decision
         document.getElementById(`choiceBox`).innerText = previousResponse
     }
 
@@ -56,34 +68,33 @@ function changeDial(tempPath, previousResponse) {
         }
         addElement(`button`, `response1`, `Move on`, `textArea`, dialogue[tempPath], goTo)
     } else {
-        if (dialogue[tempPath].getNote !== undefined) {
+        if (dialogue[tempPath].getNote !== undefined) { // check if player found the note in the cabinet earlier
             noteFound = true;
-            console.log("note found");
         }
         for (i = 1; i < dialogue[tempPath].responses + 1; i++) {
             //console.log(dialogue[tempPath].responses)
             let goTo = `goTo` + i;
-            console.log(goTo);
             goTo = dialogue[tempPath][goTo];
-            console.log(goTo);
-            if (i === 2 && goTo === undefined) {
+            let img = `image` + i;
+            if (i === 2 && goTo === undefined) { // if there is no 2nd navigation variable, default to the first
                 goTo = dialogue[tempPath].goTo1
             }
 
             let choice = `response` + i + `Choice`;
             //console.log(choice);
-            addElement(`button`, `response` + i, dialogue[tempPath][choice], `textArea`, dialogue[tempPath], goTo)
+            addElement(`button`, `response` + i, dialogue[tempPath][choice], `textArea`, dialogue[tempPath], goTo, img)
         }
     }
 }
 
 
-function addElement(tag, id, content, afterEl, path, goTo) {
+function addElement(tag, id, content, afterEl, path, goTo, img) {
     // create a new element
     const newEl = document.createElement(tag);
     newEl.id = id;
     newEl.class = "button"
-    newEl.addEventListener("click", (e) => changeDial(goTo, path[id]));
+    newEl.value = img
+    newEl.addEventListener("click", (e) => changeDial(goTo, path[id], path[img]));
 
     // and give it some content
     newEl.innerText = content;
